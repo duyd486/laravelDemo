37b0d44e0cb78a17bcaa80553bf13f81f4a9e067
duyd486 <90164590+duyd486@users.noreply.github.com>
2025-05-10 04:31:28 +0700

    add

---
+++ database/seeders/UserStatusSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \DB::table("user_status")->insert([
+            ["name" => "Hoạt động"],
+            ["name" => "Tạm khóa"]
+        ]);
+    }
+}
